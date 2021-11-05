1b109c9e9807d1e01edb65e313a3fbe5c45699a7
javierpb275 <javierpb275@gmail.com>
2021-11-05 18:52:44 +0100

    - added questionsAnswered to data in App.vue. - used questionsAnswered to render either questions component or result component using a v-if directive and v-else. -passed questions as porps to questions component. - in Questions.vue component, used the questions props to loop thru them and thru the answers using the v-for directive.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="ctr">
-    <questions />
-    <result />
+    <questions
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+    />
+    <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
@@ -18,6 +21,7 @@ export default {
   },
   data() {
     return {
+      questionsAnswered: 0,
       questions: [
         {
           q: "What is 2 + 2?",
