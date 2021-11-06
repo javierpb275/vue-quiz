42d3f6fef74c076a49943b9ccfed30bdd72049c8
javierpb275 <javierpb275@gmail.com>
2021-11-06 17:03:31 +0100

    execute function reset in button reset and show button if questionsAnswered is equal to questions.length

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
       @question-answered="questionAnswered"
     />
     <result v-else :results="results" :totalCorrect="totalCorrect" />
-    <button type="button" class="reset-btn">Reset</button>
+    <button
+      type="button"
+      class="reset-btn"
+      @click.prevent="reset"
+      v-if="this.questionsAnswered === questions.length"
+    >
+      Reset
+    </button>
   </div>
 </template>
 
@@ -108,6 +115,10 @@ export default {
         this.totalCorrect++;
       }
       this.questionsAnswered++;
+    },
+    reset() {
+      this.questionsAnswered = 0;
+      this.totalCorrect = 0;
     },
   },
 };
